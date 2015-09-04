d84498d837216e8129a71df15ce6455394ceb543
lixiaojun <lixiaojun@ucloud.cn>
2015-09-04 15:31:59 +0800

    init

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 /**
- *
  * Created by rilke on 15/8/24.
+ * /js error  | api error | 前端性能 | 点击流
  */
 
 var express = require('express');
@@ -9,19 +9,21 @@ var log4js = require('log4js');
 
 var app = express();
 
-log4js.configure()
+log4js.configure('log4js.config.json',{reloadSecs:300});
+
 app.listen(80);
 
 //CORS middleware
 var allowCrossDomain = function(req, res, next) {
-	res.header('Access-Control-Allow-Origin', 'example.com');
+	res.header('Access-Control-Allow-Origin', '*');
 	res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
 	res.header('Access-Control-Allow-Headers', 'Content-Type');
 
 	next();
-}
+};
 
 app.use(allowCrossDomain);
-app.get('/error',function(req,res){
 
-})
+app.all('/error',function(req,res){
+
+});
